0f2556148b3a2a03f1040c6fdd75b521ce94dff4
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2014-08-29 17:38:20 +0200

    Add TIE dummy prototype

---
+++ pyoptics/TIE.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+def _div_of_grad():
+    pass
+
+
+def _inv_laplacian():
+    pass
+
+
+class TIE(object):
+    def __init__(self, I_stack, deriv_order, reg_constant):
+        pass
+
+
+class TIE_iterated(object):
+    # repeat TIE as in L. Waller's paper
+    pass
