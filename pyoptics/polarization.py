211836d65a35c81528ab1b341aebd76e934f2336
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-01-20 21:24:51 +0100

    New module suggestions

---
+++ pyoptics/polarization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""Tools for computations involving polarization.
+"""
+
+
+def lin_pol_to_TE_TM(field):
+    """Convert [E_x, E_y] data to [E_TE, E_TM].
+    """
+
+    pass
+
+
+def TE_TM_pol_to_lin(field):
+    """Convert [E_TM, E_TM] data to [E_x, E_y].
+    """
+
+    pass
+
+
+
+def IPS(field, pref_state):
+    pass
+
+
+def DOP(field):
+    pass
---
+++ pyoptics/thin_films.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+"""Thin film reflection and transmission coefficients.
+"""
+
+pass
