423df9b939616a6aa891d9edaeb808399cd4e130
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-02-07 18:10:48 +0100

    Make thin film code more modular
    Allow use of tilted impedances in R,T,A routine as well. T gets a
    correction that depends on tilted impedances.

--- pyoptics/thin_films.py
+++ pyoptics/thin_films.py
@@ -4,6 +4,7 @@
 """Thin film reflection and transmission coefficients.
 """
 
+from copy import copy
 from functools import reduce  # Python 3 compatibility
 
 import numpy as np
@@ -12,6 +13,50 @@ from numpy import sin, cos
 
 from utils import Z_0 as Z_vac, wavenumber
 from interfaces import refracted_angle
+
+
+def _n_k_arrays(n, k, embedding_n, embedding_k, substrate_n, substrate_k):
+
+    n = np.asarray( [embedding_n] + list(n) + [substrate_n] )
+    k = np.asarray( [embedding_k] + list(k) + [substrate_k] )
+
+    return n, k
+
+
+def _impedances(complex_n):
+    return Z_vac/complex_n
+
+
+def _cos_theta(theta_0, complex_n):
+    """Compute cosine of complex propagation angles in all media.
+    """
+
+    ##note: cos(theta) = sqrt(1 - sin(theta)); sin(theta) is found from Snell's law:
+    ##TODO: why complex_n[0]/complex_n[i] and not complex_n[i-1]/complex_n[i]?
+    #cos_theta = complex_sqrt(1.0-(complex_n[0])/complex_n*sin(angle))**2
+
+    # compute angles in layers using Snell's law:
+    angles = [ theta_0, ]
+    for i in range(1, len(complex_n)):
+        angles.append( refracted_angle(angles[i-1], complex_n[i-1], complex_n[i]) )
+    cos_theta = cos(angles)
+
+    # FIXME: which cos_theta is to use?
+
+    return cos_theta
+
+
+def _tilted_Z(Z_in, cos_theta, pol):
+    Z = copy(Z_in)
+    # compute 'tilted' addmittances:
+    if(pol in ("s", "TE")):
+        Z *= cos_theta
+    elif(pol in ("p", "TM")):
+        Z /= cos_theta
+    else:
+        raise ValueError("'pol' must be on of 's'/'TE', 'p'/'TM'")
+
+    return Z
 
 
 def r_t_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
@@ -42,48 +87,21 @@ def r_t_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
     assert(len(k) >= 1)
     assert(len(t) == len(n) == len(k))
 
-    n = np.asarray( [embedding_n] + list(n) + [substrate_n] )
-    k = np.asarray( [embedding_k] + list(k) + [substrate_k] )
-
+    n, k = _n_k_arrays(n, k, embedding_n, embedding_k, substrate_n, substrate_k)  # extend n, k arrays by embedding medium and substrate
     complex_n = n + 1j*k
-    Z = Z_vac/complex_n  # complex impedance for all media
+    Z = _impedances(complex_n)  # complex impedance for all media
+    cos_theta = _cos_theta(angle, complex_n)  # complex propagation angles in all media
+    Z = _tilted_Z(Z, cos_theta, pol)  # McLeod's 'tilted admittances'
 
-    # build A B C D matrices for each layer:
-    N_max = len(n) - 1
-
-    ##note: cos(theta) = sqrt(1 - sin(theta)); sin(theta) is found from Snell's law:
-    ##TODO: why complex_n[0]/complex_n[i] and not complex_n[i-1]/complex_n[i]?
-    #cos_theta = complex_sqrt(1.0-(complex_n[0])/complex_n*sin(angle))**2
-
-    # FIXME: which angle thingy is correct?
-
-    # compute angles in layers using Snell's law:
-    angles = [ angle, ]
-    for i in range(1, N_max+1):
-        angles.append( refracted_angle(angles[i-1], complex_n[i-1], complex_n[i]) )
-    #angles = np.asarray(angles)
-    cos_theta = np.cos(angles)
-
-    # FIXME: which cos_theta is to use?
-
-    # compute 'tilted' addmittances:
-    if(pol in ("s", "TE")):
-        Z *= cos_theta
-    elif(pol in ("p", "TM")):
-        Z /= cos_theta
-    else:
-        raise ValueError("'pol' must be on of 's'/'TE', 'p'/'TM'")
-
-    # construct transfer matrices for all layers:
+    # construct Zangwill's transfer matrices for all layers:
     M = []
-    for j in range(1, N_max):
+    for j in range(1, len(n) - 1):
         phi_j = t[j-1]*wavenumber(wavelength, complex_n[j])*cos_theta[j]  # 'phase' accumulated in layer j
 
         M_curr = np.array( [ [cos(phi_j), -1j*Z[j]*sin(phi_j)], [-1j/Z[j]*sin(phi_j), cos(phi_j)] ] )
         M.append(M_curr)
 
     M_total = reduce(np.dot, M)  # multiply all M matrices
-    # TODO: order of multiplication?
 
     A, B, C, D = M_total.flatten()  # unpack layer stack matrix
 
@@ -97,7 +115,7 @@ def r_t_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
 def R_T_A_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
                  substrate_k, pol, wavelength):
     """Same as r_t_coeffs, but for intensity reflectance and transmittance. Also
-    return 'absorptance' such that R + T + A = 1.
+    return 'absorbtance' such that R + T + A = 1.
 
     Note
     ----
@@ -108,8 +126,15 @@ def R_T_A_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
     r, t = r_t_coeffs(angle, n, k, t, embedding_n, embedding_k, substrate_n,
                       substrate_k, pol, wavelength)
 
+    # prepare data for T correction:
+    n, k = _n_k_arrays(n, k, embedding_n, embedding_k, substrate_n, substrate_k)
+    complex_n = n + 1j*k
+    Z = _impedances(complex_n)  # complex impedance for all media
+    cos_theta = _cos_theta(angle, complex_n)
+    Z = _tilted_Z(Z, cos_theta, pol)
+
     R = np.abs(r)**2
-    T = substrate_n/embedding_n*np.abs(t)**2  # TODO: can we interpret the prefactor as a radiometric correction?
+    T = np.real(Z[-1])/np.real(Z[0])*np.abs(t)**2  # TODO: can we interpret the prefactor as a radiometric correction?
     A = 1.0 - R - T
 
     return R, T, A
@@ -135,7 +160,7 @@ if(__name__ == '__main__'):
     n = np.array([ AlOx_n, Al_n, Ag_n ] )
     k = np.array([ AlOx_k, Al_k, Ag_k ] )
 
-    AOI_vals = np.linspace(0, 90, 200)
+    AOI_vals = np.linspace(0, 40, 200)
 
     R_p_vals, R_s_vals, T_s_vals, T_p_vals = [], [], [], []
     for AOI in AOI_vals:
