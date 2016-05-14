d4a2b7d9ca2d8b51be6dcf33a44ae8c260fd9ca1
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-05-14 13:38:13 +0200

    Make WPM less memory-consuming, also speed-up
    - less meshed data
    - hopefully less copied data
    - less C calls

--- pyoptics/wpm.pyx
+++ pyoptics/wpm.pyx
@@ -18,11 +18,11 @@ import matplotlib.pyplot as plt
 
 import numexpr as ne  # TODO: make optional?
 
-from .utils import freq_grid, wavenumber, k_z, TWOPI
+from .utils import frequencies, wavenumber, k_z, TWOPI
 from .fft import FT, FT_unitary
 
 
-cpdef wpm_propagate(n_func, field_z, x, y, z, delta_z, wl):
+cpdef wpm_propagate(n_func, field_z, double[:] x, double[:] y, double z, double delta_z, double wl):
     """Propagate a field using WPM.
 
     Parameters
@@ -34,39 +34,42 @@ cpdef wpm_propagate(n_func, field_z, x, y, z, delta_z, wl):
     -------
     field_propagted : array
         Field propagated to z_new = z + delta_z.
+
+    Note
+    ----
+    For N x-samples and M y-samples, the WPM scales with O(N^2*M^2).
     """
 
     cdef Py_ssize_t i, j, m, n
     cdef np.ndarray[ndim=2, dtype=double complex] field_propagted = \
         np.zeros(np.shape(field_z), dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=double complex] n_sampled, field_freq_space
-    cdef np.ndarray[ndim=2, dtype=double] K_x, K_y, X, Y
+    cdef np.ndarray[ndim=2, dtype=double] X, Y
+    cdef double[:] k_x, k_y
     cdef double complex kz
-    cdef double dz, k0, curr_x, curr_y, curr_kx, curr_ky
-    cdef double complex imag_unit = complex(0, 1)
-
-    dz = delta_z
+    cdef double k0, curr_x, curr_y, curr_kx, curr_ky
+    cdef double complex imag_unit = complex(0.0, 1.0)
 
     k0 = wavenumber(wl)
-    X, Y = np.meshgrid(x, y)
 
+    X, Y = np.meshgrid(x, y)  # TODO: make n_sampled local in x, y (attention! works nicely with cpdef'ed funcs)
     n_sampled = np.asarray(n_func(X, Y, z), dtype=complex)
 
-    K_x, K_y = freq_grid(x, y, wavenumbers=True, normal_order=True)
+    k_x, k_y = frequencies(x, wavenumbers=True, normal_order=True), frequencies(y, wavenumbers=True, normal_order=True)
 
     field_freq_space = FT_unitary(field_z)
 
     # iterate all over spatial pixels, perform k summation for each pixel:
     for i in prange(n_sampled.shape[0], nogil=True, schedule="dynamic"):
         for j in range(n_sampled.shape[1]):
-            curr_x, curr_y = X[i, j], Y[i, j]
-            for m in range(K_x.shape[0]):
-                for n in range(K_x.shape[1]):
-                    curr_kx, curr_ky = K_x[m, n], K_y[m, n]
+            curr_x, curr_y = x[j], y[i]
+            for m in range(k_x.shape[0]):
+                for n in range(k_y.shape[0]):
+                    curr_kx, curr_ky = k_x[n], k_y[m]
                     kz = csqrt((n_sampled[i, j]*k0)**2 - curr_kx**2  - curr_ky**2)
                     field_propagted[i, j] += (field_freq_space[m, n]  # plane wave amplitude
-                                              * cexp(imag_unit*(curr_kx*curr_x + curr_ky*curr_y))  # transversal part
-                                              * cexp(imag_unit*dz*kz)  # propagating component
+                                              # transversal part and propagating part:
+                                              *cexp(imag_unit*((curr_kx*curr_x + curr_ky*curr_y) + delta_z*kz))
                                               )
 
     # TODO: truncate loop over frequencies if non-propagating frequencies occur?
