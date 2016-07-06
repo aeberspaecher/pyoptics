43a9e62045de36304f739f8176c3aa490f86258d
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-07-07 01:10:39 +0200

    plot_tools improvements
    - facecolor is now white
    - aspect ratio is forced again to equal
    - reduce number of ticks to avoid crowded plots

--- pyoptics/plot_tools.py
+++ pyoptics/plot_tools.py
@@ -44,6 +44,11 @@ def plot_intensity(psi, x, y, xlabel=None, ylabel=None, title=None, colorbar=Tru
     if "cmap" not in kwargs:
         kwargs["cmap"] = default_cmap
 
+    fig = plt.figure(facecolor="white")
+    ax = plt.gca()
+
+    ax.locator_params(nbins=5)
+
     plt.imshow(psi, extent=(x[0], x[-1], y[0], y[-1]), origin="lower",
                **kwargs)
     if xlabel is not None:
@@ -87,12 +92,13 @@ def plot_field(field, x, y, xlabel=None, ylabel=None, title=None, colorbar=True,
 
     # FIXME: aspect ratio & extent - images can be truncated!
 
-    fig = plt.figure()
+    fig = plt.figure(facecolor="white")
 
     if colorbar and horizontal_layout:
         gs = gridspec.GridSpec(1, 2,
                                #width_ratios=[1, 1],
                                #height_ratios=[1],
+                               wspace=0, hspace=0
                               )
 
         amp_ax = fig.add_subplot(gs[0])
@@ -104,12 +110,12 @@ def plot_field(field, x, y, xlabel=None, ylabel=None, title=None, colorbar=True,
         phase_cbar_ax = phase_div.append_axes("right", size="10%", pad=0.05)
         plt.setp(phase_ax.get_yticklabels(), visible=False)
     if (not colorbar) and horizontal_layout:
-        gs = gridspec.GridSpec(1, 2, width_ratios=[10, 10], height_ratios=[1])
+        gs = gridspec.GridSpec(1, 2)
         amp_ax = fig.add_subplot(gs[0, 0])
         phase_ax = fig.add_subplot(gs[0, 1], sharey=amp_ax)
         plt.setp(phase_ax.get_yticklabels(), visible=False)
     if colorbar and (not horizontal_layout):
-        gs = gridspec.GridSpec(2, 1)#, width_ratios=[10, 0.5], height_ratios=[1, 1])
+        gs = gridspec.GridSpec(2, 1)
         amp_ax = fig.add_subplot(gs[0, 0])
         phase_ax = fig.add_subplot(gs[1, 0], sharex=amp_ax)
         amp_div = make_axes_locatable(amp_ax)
@@ -118,13 +124,16 @@ def plot_field(field, x, y, xlabel=None, ylabel=None, title=None, colorbar=True,
         phase_cbar_ax = phase_div.append_axes("right", size="10%", pad=0.05)
         plt.setp(amp_ax.get_xticklabels(), visible=False)
     if (not colorbar) and (not horizontal_layout):
-        gs = gridspec.GridSpec(2, 1, width_ratios=[10], height_ratios=[1, 1])
+        gs = gridspec.GridSpec(2, 1)
         amp_ax = fig.add_subplot(gs[0, 0])
         phase_ax = fig.add_subplot(gs[1, 0], sharex=amp_ax)
         plt.setp(amp_ax.get_xticklabels(), visible=False)
 
     if not np.iscomplexobj(psi):
         raise ValueError("psi must be complex-valued")
+
+    amp_ax.locator_params(nbins=5)
+    phase_ax.locator_params(nbins=5)
 
     amp = I(field, n) if use_intensity else np.abs(field)
     phase = np.angle(field) if use_rad else np.angle(field)/np.pi
@@ -133,14 +142,14 @@ def plot_field(field, x, y, xlabel=None, ylabel=None, title=None, colorbar=True,
         kwargs["cmap"] = default_cmap
 
     im1 = amp_ax.imshow(amp, extent=(x[0], x[-1], y[0], y[-1]), origin="lower",
-                        **kwargs)
+                        aspect="equal", **kwargs)
 
     if "phase_cmap" not in kwargs:
         kwargs["cmap"] = default_cmap_phase
     else:
         kwargs["cmap"] = kwargs.pop("phase_cmap")
 
-    im2 = phase_ax.imshow(phase, extent=(x[0], x[-1], y[0], y[-1]),
+    im2 = phase_ax.imshow(phase, extent=(x[0], x[-1], y[0], y[-1]), aspect="equal",
                           origin="lower", **kwargs)
 
     if amp_title is not None:
@@ -171,7 +180,6 @@ def plot_field(field, x, y, xlabel=None, ylabel=None, title=None, colorbar=True,
 
     amp_ax.set(adjustable='box-forced', aspect="equal")
     phase_ax.set(adjustable='box-forced', aspect="equal")
-    #gs.tight_layout(fig, h_pad=1, w_pad=1)
 
     gs.tight_layout(fig, rect=[0, 0.03, 1, 0.95])
 
@@ -183,10 +191,10 @@ if(__name__ == '__main__'):
     from pyoptics.beams import gauss_laguerre, gauss_hermite
     from pyoptics.utils import grid1d
     wl = 0.630
-    x, y = grid1d(-2, +2, 512, True), grid1d(-2, 2, 512, True)
-    psi = gauss_laguerre(0, 10 , x, y, z=-0.01, w_0=0.5, z_r=1.1, wl=wl)
+    x, y = grid1d(-2, +2, 512, False), grid1d(-2, 2, 512, False)
+    psi = gauss_laguerre(5, 10, x, y, z=-0.01, w_0=0.5, z_r=1.1, wl=wl)
 
-    #plot_intensity(psi, x, y)
+    plot_intensity(psi, x, y)
     plot_field(psi, x, y, title="Test", amp_title="Intensity",
                phase_title=r"Phase [$\pi$]", use_rad=False,
                xlabel="$x$ [arb. units]", ylabel="$y$ [nm]",
