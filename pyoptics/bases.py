514cfa8185823fb5457fec323e959c638f0cd22f
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-07-18 23:12:20 +0200

    New features in bases module
    - Basis objects are now callable to return linear combinations of basis
       functions
    - expansion functions added:
    * least sqaures fitting
    * direct integration
    - interface cleanup
    - Fringe Zernike normaliation factor added

--- pyoptics/bases.py
+++ pyoptics/bases.py
@@ -12,8 +12,9 @@ from math import sqrt, floor, ceil
 
 import numpy as np
 from scipy.misc import factorial as fac  # tolerant to negative arguments!
+from scipy.linalg import lstsq
 
-from pyoptics.utils import scalar_product
+from pyoptics.utils import scalar_product, kronecker_delta, simpson_weights
 
 
 class BasisSet(object):
@@ -23,23 +24,26 @@ class BasisSet(object):
     # domains, e.g. the Zernike polynomials that have a circular domain of
     # definition. the specific mask is used in scalar products.
 
+    # TODO: give __mul__() to evaluated objects (scalar product)?
+
     def __init__(self, x, y):
         """Define grid the basis functions are defined on.
         """
-        # TODO: implement here or in subclasses?
         self.x = x
         self.y = y
         self.XX, self.YY = np.meshgrid(x, y)
+        self._has_norm = False
 
-    def __call__(self, coeffs):
-        """Return a field computed from the given expansion coefficients.
+    def __call__(self, indices, coeffs=None):
+        """Return basis functions for given indices.
         """
 
-        # distinguish scalar coeff (np.isscalar) and array
+        if coeffs is None:
+            coeffs = np.ones(len(indices))
 
-        # TODO: give __mul__() to evaluated objects (scalar product)?
+        val = sum([coeffs[i]*self.eval_single(ind) for i, ind in enumerate(indices)])
 
-        pass
+        return val
 
     def eval_single(self, index):
         """Return a single basis function.
@@ -47,32 +51,79 @@ class BasisSet(object):
 
         pass
 
-    def coeffs_from_field(self, field, method="LSTSQ"):
+    def coeffs_from_field(self, field, indices, method="LSTSQ"):
         """Expand a given field in the basis set.
         """
 
-        # TODO: offer expansion by least square fits and expansion by evaluation of scalar products
+        if method == "LSTSQ":
+            coeffs = self._coeffs_from_field_lstsq(field, indices)
+        elif method == "DI":
+            coeffs = self._coeffs_from_field_integration(field, indices)
+        else:
+            raise ValueError("Unknown method. Must be one of 'LSTSQ' or 'DI'")
 
-        pass
+        return coeffs
 
-    def scalar_product(self, field, index):
-        """Compute the scalar product of a given field and a given basis
-        function.
+    def _coeffs_from_field_lstsq(self, field, indices):
+        """Determine basis expansion coefficients by least-square fitting.
 
-        The implemenation here allows to use scalar products with different
-        weight functions.
+        Parameters
+        ----------
+        field : array
+            Sampled data to expand in basis.
+        indices : array, integer
+            Indices to consider in fit.
+
+        Returns
+        -------
+        coeffs : array
+            Expansion coefficients.
+
+        Note
+        ----
+        Least square fitting may be better than direct integration as it tries
+        to optimally describe the given field. However, balancing the basis
+        functions for minimum residual can be unphysical, specifically if the
+        sampling is questionable. However, least squares generally give very
+        good results and are recommended generally.
         """
 
-        # TODO: all scalar products are the same - except for the weight function!
-        # implement only the weight function and extend the scalar_product() in
-        # the utils module by an optional weight function
+        # TODO: preconditioning?
+        Z_i = [self.eval_single(i) for i in indices]
+        N_samples = len(Z_i[0].flatten())
+        N_indices = len(indices)
+        M  = np.zeros([N_samples, N_indices])
+        for i in range(N_indices):
+            M[:, i] = Z_i[i].flatten()
+        coeffs, _, _, _ = lstsq(M, field.flatten())
 
-        # TODO: implement standard scalar product in base class, override in
-        # subclasses
+        return coeffs
 
-        # scalar_product() is available in utils
+    def _coeffs_from_field_integration(self, field, indices):
+        """Determine basis expansion coefficients by direct overlap integral
+        computation.
 
-        pass
+        Parameters
+        ----------
+        field : array
+            Sampled data to expand in basis.
+        indices : array, integer
+            Indices to consider in fit.
+
+        Returns
+        -------
+        coeffs : array
+            Expansion coefficients.
+        """
+
+        if self._has_norm:
+            norms = [self.normalization_factor(i)  for i in indices]
+        else:
+            norms = len(indices)*(1.0,)
+
+        coeffs = 1.0/np.array(norms)*np.array([scalar_product(self.eval_single(i), self.mask*field, self.x, self.y, simpson_weights) for i in indices])
+
+        return coeffs
 
 
 class PreSampledBasisSet(BasisSet):
@@ -91,19 +142,17 @@ class FringeZernikes(BasisSet):
         self.Phi = np.arctan2(self.YY, self.XX)
 
         mask = np.zeros(np.shape(self.XX))
-        mask[self.Rho < 1.0] = 1.0
+        mask[self.Rho <= 1.0] = 1.0
         self.mask = mask
+        self._has_norm = True
 
     def R_nm(self, n, m):
         # TODO: factor out?
-        #summands = [(-1)**k * binom(n-k, k) * binom(n-2*k, (n-m)//2.-k) * self.Rho**(n-2.0*k) for k in range(0, int((n-m)//2 + 1))]
-        #R = sum(summands)
-
         N, M = int(n), int(m)
 
         R = np.zeros_like(self.Rho)
         for k in range((N-M)/2 + 1):
-            R += (-1.0)**k * fac(N-k) / ( fac(k) * fac( (N+M)/2.0 - k ) * fac( (N-M)/2.0 - k ) ) * self.Rho**(n-2.0*k)
+            R += (-1.0)**k * fac(N-k) / (fac(k) * fac((N+M)/2.0 - k) * fac((N-M)/2.0 - k )) * self.Rho**(n-2.0*k)
 
         return R
 
@@ -123,8 +172,6 @@ class FringeZernikes(BasisSet):
         m = floor((((d**2-j)/2)) if (((int(d)**2 - j) % 2) == 0) else ceil((-(d**2) + j - 1)/2.))
         n = round((2.0*(d-1) - abs(m)))
 
-        print("j = {}: n = {}; m = {}; d = {}".format(j, n, m, d))
-
         return n, m
 
     def eval_single(self, j):
@@ -133,6 +180,30 @@ class FringeZernikes(BasisSet):
         Y = self.Y_m(m)
 
         return R*Y*self.mask
+
+    def normalization_factor(self, i):
+        """Norm <Z_i|Z_i> of i-th Fringe-Zernike polynomial.
+
+        Parameters
+        ----------
+        i : int
+
+        Returns
+        -------
+        norm : double
+
+        Note
+        ----
+        The Fringe Zernikes get their normalization from the choice of
+        R(R_max) = 1. This means the norm over the entire (unit) disk may
+        differs with the Fringe index.
+        """
+
+        n, m = self.fringe_to_n_m(i)
+
+        norm = self.R_norm**2*np.pi*(1.+kronecker_delta(m, 0))/(2.*(n + 1.))
+
+        return norm
 
 
 class Polynomials(BasisSet):
