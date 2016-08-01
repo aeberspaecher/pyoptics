3e21909098c1ea4b9222ebfcedd6e92ac584a645
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2016-08-01 23:58:38 +0200

    First Legendre basis set implementation

--- pyoptics/bases.py
+++ pyoptics/bases.py
@@ -5,16 +5,25 @@
 
 In optics, different basis sets are used to describe fields in the plane, e.g.
 Zernike polynomials in the description of aberrations in systems with circular
-aperture stops. This module offers an object oriented approach such basis sets.
+aperture stops. This module offers an object oriented approach to such basis
+sets.
 """
 
 from math import sqrt, floor, ceil
 
 import numpy as np
+from numpy.polynomial.legendre import legval2d, legval
 from scipy.misc import factorial as fac  # tolerant to negative arguments!
 from scipy.linalg import lstsq
 
-from pyoptics.utils import scalar_product, kronecker_delta, simpson_weights
+from pyoptics.utils import scalar_product, kronecker_delta
+
+# TODO: reintroduce scalar/inner product in BasisSet? this allowed for inner products that respected the mask/support automatically
+# TODO: in case of scaled x, y supports (Zernike R_norm or Legdendre rectangle size): which x, y to store? Scaled or unscaled? Compute scalar products in which coordinates?
+
+
+# probably keep original coordinates and use those in all computations, but also store transform x, y for eval_single().
+# this allows reuse of x, y by the user and will shift responsibilty away from the user to the developer!
 
 
 class BasisSet(object):
@@ -50,6 +59,12 @@ class BasisSet(object):
         """
 
         pass
+
+    def normalization_factor(self, i):
+        """Return norm of i-th basis function. This is used in basis expansions.
+        """
+
+        raise NotImplementedError("Do not call this function in base class.")
 
     def coeffs_from_field(self, field, indices, method="LSTSQ"):
         """Expand a given field in the basis set.
@@ -127,7 +142,17 @@ class BasisSet(object):
 
 
 class PreSampledBasisSet(BasisSet):
-    pass
+    def __init__(self, x, y, indices):
+         pass
+
+    def eval_single(self, index):
+        if index not in self.sampled_indices:
+            raise ValueError("Index {} not amongst the sampled indices".format(index))
+
+        # TODO: implement
+        pass
+
+
 
 
 class FringeZernikes(BasisSet):
@@ -206,6 +231,108 @@ class FringeZernikes(BasisSet):
         return norm
 
 
+class LegendrePolynomials(BasisSet):
+    """Legendre polynomials are a suitable basis set for fields defined on
+    rectangular apertures.
+
+    The basis set is the product of two Legendre polynomials L_i(x)*L_j(y).
+
+    """
+
+    def __init__(self, x, y, a, b, x0=0.0, y0=0.0):
+        super(LegendrePolynomials, self).__init__((x-x0)/a, (y-y0)/b)
+        self.a = a
+        self.b = b
+        self.x0 = x0
+        self.y0 = y0
+        self._has_norm = True
+
+        mask = np.zeros_like(self.XX)
+        mask[
+             (self.XX >= -1) & (self.XX <= +1)
+             & (self.YY >= -1) & (self.YY <= +1)
+            ] = 1.0
+        self.mask = mask
+
+    def eval_single(self, index):
+        n, m = self.single_index_to_n_m(index)
+        c = np.zeros(2*(max(n, m)+1,))
+        c[n, m] = 1.0  # using legval2d, row index goes to x, column index to y
+
+        #poly_x = legval((self.x-self.x0)/self.a)  # TODO: scaling
+        #poly_y = legval((self.y-self.y0)/self.b)
+        xy_poly = legval2d(self.XX, self.YY, c)
+        #xy_poly = np.outer(poly_x, poly_y)  # TODO: order?
+
+        return self.mask*xy_poly
+
+    def single_index_to_n_m(self, index):
+        """Convert single index to indices n, m for L_n(x) and L_m(y).
+        """
+
+        n, m = _poly_single_index_to_n_m(index)
+
+        return n, m
+
+
+    def normalization_factor(self, i):
+        """Norm <L_i|L_i> of i-th Legendre basis function.
+
+        Parameters
+        ----------
+        i : int
+
+        Returns
+        -------
+        norm : double
+        """
+
+        n, m = self.single_index_to_n_m(i)
+
+        norm = 2./(2*n + 1.)  # TODO: respect rectangle area! scale by area?
+
+        return norm
+
+
+    # TODO: scaling function that maps (-a/2-x0, +a/2-x0) to (-1, +1)
+
+
+
+def _poly_single_index_to_n_m(single_ind):
+    """Map single index to x, y indices/powers.
+
+    n and m may be used as powers in monomials (Polynomials class) or as indices
+    for e.g. Legendre polynomials.
+
+    """
+
+    # find order of monomial: single_ind >= (order+1)*(order+2)
+    # solve expression for number of monomials up to given order for order:
+    order_fract = int(-3./2 + sqrt(9./4 - 2*(1-single_ind)) + 1)
+    order = int(floor(order_fract))
+
+    element = single_ind - poly_basis_size(order-1)  # which element in the sequence of monomials up to given order?
+    elements_in_order = order + 1
+
+    n = order - element
+    m = element
+
+    return n, m
+
+
+
+def poly_basis_size(order):
+    """Number of monomials up to given order.
+    """
+
+    if order < 0:
+        s = 0  # account for order 0
+    else:
+        s = (order+1)*(order+2)/2
+
+    return s
+
+
 class Polynomials(BasisSet):
     """Polynomials in x and y.
     """
@@ -213,13 +340,7 @@ class Polynomials(BasisSet):
     pass
 
 
-def poly_basis_size(order):
-    # Number of nonomials up to given order.
-
-    pass
-
-
-class NumericallyOrthogonalized(BasisSet):
+class NumericallyOrthogonalized(PreSampledBasisSet):
     """Represents a basis set that is numerically orthogonalized through a
     Gram-Schmidt procedure on the given grid.
     """
