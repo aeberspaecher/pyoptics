40c3f9484f46b1573797a1494106762c0749b6b9
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2017-10-16 22:06:00 +0200

    WPM updates (doc, imports, protype, test function)
    - improve docstring
    - fix imports
    - remove obsolete comments (trapezoidal integration beats Simpson for
    oscillatory integrands)
    - outline API for averaging_sampling()
    - fix demo/test function
    
    Adresses #1.

--- pyoptics/wpm.pyx
+++ pyoptics/wpm.pyx
@@ -4,31 +4,61 @@
 """Wave Propagtion method of Singer / Brenner.
 """
 
-import numpy as np
-cimport numpy as np
-#from libcpp.complex cimport sqrt as complex_sqrt, exp as complex_exp
 
 cdef extern from "complex.h":
     double complex csqrt(double complex) nogil
     double complex cexp(double complex) nogil
 
+
+import numpy as np
+cimport numpy as np
+
 from cython.parallel import parallel, prange
 
-import matplotlib.pyplot as plt
-
-import numexpr as ne  # TODO: make optional?
-
-from .utils import frequencies, wavenumber, k_z, TWOPI
-from .fft import FT, FT_unitary
+from .utils import frequencies, wavenumber, k_z
+from .fft import FT_unitary, inv_FT_unitary
 
 
-cpdef wpm_propagate(n_func, field_z, double[:] x, double[:] y, double z, double delta_z, double wl):
+def averaging_sampling(func, x, delta_x, N):
+    """Average func(x) between x and x + delta_x by performing the integral
+    1/delta_x \int_{x}^{x+dx} func(x) dx
+    numerically.
+
+    Parameters
+    ----------
+    func : callable
+        f(x)
+    x, delta_x : double
+    N : int
+        Number of samples to use in Gaussian quadrature.
+
+    Returns
+    -------
+    avg : same type as f(x)
+        Averaged integral over f(x)
+    """
+
+    pass
+
+
+cpdef wpm_propagate(n_func, field_z, double[:] x, double[:] y, double z,
+                    double delta_z, double wl):
     """Propagate a field using WPM.
 
     Parameters
     ----------
     n_func : callable
         Refractive index n = n(x, y, z). Needs to work with vector-valued x, y.
+    field_z : array
+        Sampled field at z = z_curr.
+    x, y : arrays
+        Coordinate arrays.
+    z : number
+        Current z position.
+    delta_z : number
+        Distance to propagte.
+    wl : number
+        Wavelength.
 
     Returns
     -------
@@ -37,47 +67,68 @@ cpdef wpm_propagate(n_func, field_z, double[:] x, double[:] y, double z, double 
 
     Note
     ----
-    For N x-samples and M y-samples, the WPM scales with O(N^2*M^2).
+    For N x-samples and M y-samples, the WPM scales with O(N^2*M^2). This fact
+    should render it close to unusable for large problems.
+
+    n_func() is called only once per routine. It is supposed to accept gridded
+    x and y data. This choice may seem more specific than calling a function
+    that evaluates n(x, y) for a single x, y tuple. However, the latter
+    approach implied N*M function calls, which is slow if n_func() is a Python
+    function. The approach we've chosen shifts the responsibility of being fast
+    to n_func() and doesn't care whether n_func() achieves being fast through
+    clever vectorization, Cythonizing or any other solution.
     """
 
-    cdef Py_ssize_t i, j, m, n
+    # NOTE: all array-like objects that enter into array-formulas with operations
+    # like squaring are defined as NumPy arrays, everything else is a MemoryView
+
+    cdef Py_ssize_t i, j, l, m
     cdef np.ndarray[ndim=2, dtype=double complex] field_propagted = \
         np.zeros(np.shape(field_z), dtype=np.complex128)
     cdef np.ndarray[ndim=2, dtype=double complex] n_sampled, field_freq_space
     cdef np.ndarray[ndim=2, dtype=double] X, Y
-    cdef double[:] k_x, k_y
-    cdef double complex kz
-    cdef double k0, curr_x, curr_y, curr_kx, curr_ky
+    cdef double[::1] k_x, k_y
+    cdef double complex kz, n_curr
+    cdef double k0, x_curr, y_curr, kx_curr, ky_curr
     cdef double complex imag_unit = complex(0.0, 1.0)
 
     k0 = wavenumber(wl)
 
-    X, Y = np.meshgrid(x, y)  # TODO: make n_sampled local in x, y (attention! works nicely with cpdef'ed funcs)
+    X, Y = np.meshgrid(x, y)
     n_sampled = np.asarray(n_func(X, Y, z), dtype=complex)
 
-    k_x, k_y = frequencies(x, wavenumbers=True, normal_order=True), frequencies(y, wavenumbers=True, normal_order=True)
+    k_x, k_y = (frequencies(x, wavenumbers=True, normal_order=True),
+                frequencies(y, wavenumbers=True, normal_order=True)
+               )
 
-    field_freq_space = FT_unitary(field_z)
+    field_freq_space = FT_unitary(field_z)  # TODO: inv_FT or FT?
 
     # iterate all over spatial pixels, perform k summation for each pixel:
     for i in prange(n_sampled.shape[0], nogil=True, schedule="dynamic"):
         for j in range(n_sampled.shape[1]):
-            curr_x, curr_y = x[j], y[i]
-            for m in range(k_x.shape[0]):
-                for n in range(k_y.shape[0]):
-                    curr_kx, curr_ky = k_x[n], k_y[m]
-                    kz = csqrt((n_sampled[i, j]*k0)**2 - curr_kx**2  - curr_ky**2)
-                    field_propagted[i, j] += (field_freq_space[m, n]  # plane wave amplitude
+            x_curr, y_curr = x[j], y[i]
+            n_curr = n_sampled[i, j]
+            # find k summation limits for this iteration's n - choose such that
+            # only propagating frequencies are chosen:
+            # TODO: implement
+            for l in range(k_y.shape[0]):
+                for m in range(k_x.shape[0]):
+                    kx_curr, ky_curr = k_x[m], k_y[l]
+                    kz = csqrt((n_curr*k0)**2 - kx_curr**2  - ky_curr**2)
+                    field_propagted[i, j] += (field_freq_space[l, m]  # plane wave amplitude
                                               # transversal part and propagating part:
-                                              *cexp(imag_unit*((curr_kx*curr_x + curr_ky*curr_y) + delta_z*kz))
+                                              *cexp(imag_unit*((kx_curr*x_curr
+                                                                + ky_curr*y_curr)
+                                                               + delta_z*kz))
                                               )
 
     # TODO: truncate loop over frequencies if non-propagating frequencies occur?
-    # TODO: can we use simpson weights to improve accuracy?
     # TODO: normalize appropriately
+    # TODO: check if sampling is appropriate - all propagating frequencies covered?
+    # TODO: add note on suggested sampling - all propagating frequencies shall be covered
 
     # TODO: so far, the algorithm assumes n changes only with x and y, but is
     # constant in z. can this condition be levered by a x,y-pixel-wise
     # integration of n over z?
 
-    return field_propagted/np.sqrt(np.prod(np.shape(field_z)))
+    return field_propagted/np.sqrt(np.prod(np.array(np.shape(field_z)) - 1))
--- test/test_wpm.py
+++ test/test_wpm.py
@@ -6,54 +6,71 @@ import matplotlib.pyplot as plt
 
 import pyoptics
 from pyoptics.wpm import wpm_propagate
+from pyoptics.propagators import *
+from pyoptics.utils import simpson_weights, weight_grid
 
 
 
-def maxwell_lens_func(x0, y0, z0, R, n_inside, n_outside):
-    def maxwell(X, Y, z):
+def spherical_lens_func(x0, y0, z0, R, n_inside, n_outside):
+    def spherical(X, Y, z):
          n_vals = np.zeros_like(X)
          inside_inds = ((X-x0)**2 + (Y-y0)**2 + (z-z0)**2 <= R**2)
-         #qimport pudb; pudb.set_trace()
-         print("Number of inside indices: {}".format(np.sum(inside_inds)))
          n_vals[inside_inds] = n_inside
          n_vals[~inside_inds] = n_outside
          return n_vals
+         #return np.ones(np.shape(X))
 
-    return maxwell
-
-
-
-Nx = 128
-Ny = 128
-Nz = 200
-
-x_max = 500
-y_max = 500
-z_max = 200
-
-maxwell = maxwell_lens_func(x0=0.0, y0=0.0, z0=100, R=50, n_inside=1.5, n_outside=1.0)
+    return spherical
 
 
-x = np.linspace(-x_max, +x_max, Nx)
-y = np.linspace(-y_max, +y_max, Ny)
+
+Nx = 71
+Ny = 71
+Nz = 700
+
+x_max = 120
+y_max = 120
+z_max = 100
+
+n_lens = 1.5
+
+spherical = spherical_lens_func(x0=0.0, y0=0.0, z0=70, R=70, n_inside=n_lens, n_outside=1.0)
+
+
+x = np.linspace(-x_max, +x_max, Nx, endpoint=True)
+y = np.linspace(-y_max, +y_max, Ny, endpoint=True)
 z = np.linspace(0, +z_max, Nz)
 
 X, Y = np.meshgrid(x, y)
 
-n = maxwell(X, Y, 100)
-
-gauss_width = 50
+gauss_width = 30
 wl = 0.630
-field_plus = np.exp(-(X**2 + Y**2)/(22*gauss_width**2))
+field_plus = np.exp(-(X**2 + Y**2)/(2*gauss_width**2))
+
+print("dz / wl", (z[1] - z[0])/wl)
+
+print("Suggested x sampling: Nx = %s"%suggest_lateral_sampling(2*x_max, n_lens, wl))
+print("Suggested y sampling: Ny = %s"%suggest_lateral_sampling(2*y_max, n_lens, wl))
+
+
+
+## compare to other diffraction routine:
+#field_propagated, _, _ = rayleigh_sommerfeld_I_IR(field_plus, x, y, z[100], wl)
+#plt.imshow(np.abs(field_propagated)**2); plt.colorbar(); plt.show()
+
 
 dz = z[1] - z[0]
 for i in range(Nz):
-    field_plus = wpm_propagate(maxwell, field_plus, x, y, z[i], dz, wl)
-    plt.imshow(np.abs(field_plus)**2); plt.show()
+    field_plus = wpm_propagate(spherical, field_plus, x, y, z[i], dz, wl)
+    n_curr = spherical(X, Y, z[i])
+    plt.imshow(n_curr, cmap=plt.cm.gray, interpolation="none"); plt.colorbar(); plt.savefig("n_%i.png"%i); plt.clf()
+    #plt.imshow(n_curr, cmap=plt.cm.gray, alpha=0.7)
+    plt.imshow(np.abs(field_plus)**2, interpolation="none"); plt.colorbar(); plt.savefig("%i.png"%i); plt.clf()#plt.show()
+    print(i)
 
 
 #plt.imshow(n[:, Ny/2, :]); plt.colorbar(); plt.show()
 
 
 
-#plt.imshow(maxwell(X, Y, 100)); plt.colorbar(); plt.show()
+#plt.imshow(spherical(X, Y, 100)); plt.colorbar(); plt.show()
